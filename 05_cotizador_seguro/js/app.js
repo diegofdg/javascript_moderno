ee80c66ef723a7e73b96391c8fd095ea09b94242
diegofdg <diegofdg@gmail.com>
2022-03-24 23:01:18 -0300

    05_cotizador_seguro-crear un prototype para mostrar un error

--- 05_cotizador_seguro/js/app.js
+++ 05_cotizador_seguro/js/app.js
@@ -20,6 +20,24 @@ UI.prototype.llenarOpciones = () => {
     }
 }
 
+UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+    const div = document.createElement('div');
+    if(tipo === 'error') {
+        div.classList.add('error');
+    } else {
+        div.classList.add('correcto');
+    }
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = mensaje;
+
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout( () => {
+        div.remove();
+    }, 3000);
+}
+
 const ui = new UI();
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -41,8 +59,9 @@ function cotizarSeguro(e) {
     const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
     if(marca === '' || year === '' || tipo === '') {
-        console.log('No pasó la validación');
-    } else {
-        console.log('Si pasó la validación');
+        ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
+        return;
     }
+
+    ui.mostrarMensaje('Cotizando...', 'exito');
 }
