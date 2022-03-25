63bfdcf82b17f93d02b33b56ea238da2c0063ba7
diegofdg <diegofdg@gmail.com>
2022-03-25 18:03:35 -0300

    06_gasto_semanal-validación del formulario

--- 06_gasto_semanal/js/app.js
+++ 06_gasto_semanal/js/app.js
@@ -3,7 +3,8 @@ const gastosListado = document.querySelector('#gastos ul');
 
 eventListeners();
 function eventListeners() {
-    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);    
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+    formulario.addEventListener('submit', agregarGasto);
 }
 
 class Presupuesto {
@@ -19,6 +20,25 @@ class UI {
         const { presupuesto, restante } = cantidad;
         document.querySelector('#total').textContent = presupuesto;
         document.querySelector('#restante').textContent = restante;
+    }
+
+    imprimirAlerta(mensaje, tipo) {        
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+        
+        if(tipo === 'error') {
+             divMensaje.classList.add('alert-danger');
+        } else {
+             divMensaje.classList.add('alert-success');
+        }
+        
+        divMensaje.textContent = mensaje;
+        
+        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+        
+        setTimeout( () => {
+             divMensaje.remove();
+        }, 3000);
     }
 }
 
@@ -36,4 +56,21 @@ function preguntarPresupuesto() {
     presupuesto = new Presupuesto(presupuestoUsuario);
 
     ui.insertarPresupuesto(presupuesto);
+}
+
+function agregarGasto(e) {
+    e.preventDefault();
+     
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = Number( document.querySelector('#cantidad').value);
+
+    if(nombre === '' || cantidad === '') {          
+          ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
+          return;
+     } else if(cantidad <= 0 || isNaN(cantidad )) {
+          ui.imprimirAlerta('Cantidad no válida', 'error')
+          return;
+     }
+     
+     console.log('Agregando gasto');
 }
