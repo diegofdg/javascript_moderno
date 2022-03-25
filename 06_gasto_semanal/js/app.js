b8427f992d458777f7b8c22c48d42e31fa70f8ce
diegofdg <diegofdg@gmail.com>
2022-03-24 23:55:35 -0300

    06_gasto_semanal-creando e instanciando la clase de presupuesto

--- 06_gasto_semanal/js/app.js
+++ 06_gasto_semanal/js/app.js
@@ -6,11 +6,28 @@ function eventListeners() {
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);    
 }
 
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
+
+class UI {}
+
+const ui = new UI();
+let presupuesto;
+
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cual es tu presupuesto?');
     console.log(Number(presupuestoUsuario));
 
     if( presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0 ) {
         window.location.reload();
-    }    
+    }
+
+    presupuesto = new Presupuesto(presupuestoUsuario);
+
+    console.log(presupuesto);
 }
