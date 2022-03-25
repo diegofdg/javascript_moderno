4d65a00c14c7d97891109b531cd1f9f39174b221
diegofdg <diegofdg@gmail.com>
2022-03-24 23:48:27 -0300

    06_gasto_semanal-primeros pasos con el proyecto

---
+++ 06_gasto_semanal/js/app.js
@@ -0,0 +1,16 @@
+const formulario = document.getElementById('agregar-gasto');
+const gastosListado = document.querySelector('#gastos ul');
+
+eventListeners();
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);    
+}
+
+function preguntarPresupuesto() {
+    const presupuestoUsuario = prompt('¿Cual es tu presupuesto?');
+    console.log(Number(presupuestoUsuario));
+
+    if( presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0 ) {
+        window.location.reload();
+    }    
+}
