09c4a50cb4462e296b9c21294bb09219e8893e3d
diegofdg <diegofdg@gmail.com>
2022-03-24 23:12:16 -0300

    05_cotizador_seguro-comenzar el calculo del seguro

--- 05_cotizador_seguro/js/app.js
+++ 05_cotizador_seguro/js/app.js
@@ -4,6 +4,36 @@ function Seguro(marca, year, tipo) {
     this.tipo = tipo;
 }
 
+Seguro.prototype.cotizarSeguro = function() {
+    let cantidad;
+    const base = 2000;
+
+    switch(this.marca){
+        case '1':
+            cantidad = base * 1.15;
+            break;
+        case '2':
+            cantidad = base * 1.05;
+            break;
+        case '3':
+            cantidad = base * 1.35;
+            break;
+        default:
+            break;
+    }
+
+    const diferencia = new Date().getFullYear() - this.year;     
+    cantidad -= ((diferencia * 3) * cantidad) / 100;
+    
+    if(this.tipo === 'basico') {
+        cantidad *= 1.30;
+    } else {
+        cantidad *= 1.50;
+    }
+    
+    return cantidad;
+}
+
 function UI() { }
 
 UI.prototype.llenarOpciones = () => {
@@ -64,4 +94,7 @@ function cotizarSeguro(e) {
     }
 
     ui.mostrarMensaje('Cotizando...', 'exito');
+
+    const seguro = new Seguro(marca, year, tipo);
+    seguro.cotizarSeguro();
 }
