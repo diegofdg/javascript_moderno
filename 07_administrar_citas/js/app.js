3de841f2b7db63582b09e6b3faa99d056ad33390
diegofdg <diegofdg@gmail.com>
2022-03-26 02:49:35 -0300

    07_administrar_citas-últimos ajustes

--- 07_administrar_citas/js/app.js
+++ 07_administrar_citas/js/app.js
@@ -1,12 +1,3 @@
-eventListeners();
+import App from './classes/App.js';
 
-function eventListeners() {
-    mascotaInput.addEventListener('input', datosCita);
-    propietarioInput.addEventListener('input', datosCita);
-    telefonoInput.addEventListener('input', datosCita);
-    fechaInput.addEventListener('input', datosCita);
-    horaInput.addEventListener('input', datosCita);
-    sintomasInput.addEventListener('input', datosCita);
-    
-    formulario.addEventListener('submit', nuevaCita);
-}
+const app = new App();
