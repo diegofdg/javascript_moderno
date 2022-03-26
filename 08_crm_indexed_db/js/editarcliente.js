a9670a1737431b87c53c7f473b08050e364b4611
diegofdg <diegofdg@gmail.com>
2022-03-26 20:29:03 -0300

    08_crm_indexed_db-llenar el formulario con la información del cliente

--- 08_crm_indexed_db/js/editarcliente.js
+++ 08_crm_indexed_db/js/editarcliente.js
@@ -1,18 +1,60 @@
 (function() {
     let DB;
+    let idCliente;
+
     const nombreInput = document.querySelector('#nombre');
+    const emailInput = document.querySelector('#email');
+    const telefonoInput = document.querySelector('#telefono');
+    const empresaInput = document.querySelector('#empresa');
+
+    const formulario = document.querySelector('#formulario');
 
     document.addEventListener('DOMContentLoaded', () => {
         conectarDB();
+        
+        formulario.addEventListener('submit', actualizarCliente);
 
         const parametrosURL = new URLSearchParams(window.location.search);
-        const idCliente = parametrosURL.get('id');
+        idCliente = parametrosURL.get('id');
         if(idCliente) {
             setTimeout( () => {
                 obtenerCliente(idCliente);
             }, 100);
         }
     });
+
+    function actualizarCliente(e) {
+        e.preventDefault();
+
+        if(nombreInput.value === '' || emailInput.value === '' || telefonoInput.value === '' || empresaInput.value === '') {
+            imprimirAlerta('Todos los campos son obligatorios', 'error');
+            return;
+        }
+
+        const clienteActualizado = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: Number(idCliente)
+        }
+
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+        objectStore.put(clienteActualizado);
+
+        transaction.oncomplete = function() {
+            imprimirAlerta('Editado Correctamente');
+
+            setTimeout( () => {
+                window.location.href = 'index.html';
+            }, 2000);
+        }
+
+        transaction.onerror = function() {
+            imprimirAlerta('Hubo un error', 'error');
+        }
+    }
 
     function obtenerCliente(id) {
         const transaction = DB.transaction(['crm'], 'readonly');
@@ -31,8 +73,11 @@
         }
 
         function llenarFormulario(datosCliente) {
-            const { nombre } = datosCliente;
+            const { nombre, email, telefono, empresa } = datosCliente;
             nombreInput.value = nombre;
+            emailInput.value = email;
+            telefonoInput.value = telefono;
+            empresaInput.value = empresa;
         }
     }
 
