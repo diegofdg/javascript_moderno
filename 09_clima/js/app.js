62bc96ed3839a21ae0fa1112e1fee5e25765509f
diegofdg <diegofdg@gmail.com>
2022-03-27 07:12:18 -0300

    09_clima-mostrando un spinner de carga

--- 09_clima/js/app.js
+++ 09_clima/js/app.js
@@ -40,6 +40,7 @@ function mostrarError(mensaje) {
 function consultarAPI(ciudad, pais) {
     const appId = 'c45b5dd4c6b0c916817eed3d608c4b11';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+    spinner();
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(datos => {
@@ -98,4 +99,28 @@ function limpiarHTML() {
     while(resultado.firstChild) {
         resultado.removeChild(resultado.firstChild);
     }
+}
+
+function spinner() {
+    limpiarHTML();
+
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('sk-fading-circle');
+    
+    divSpinner.innerHTML = `        
+        <div class="sk-circle1 sk-circle"></div>
+        <div class="sk-circle2 sk-circle"></div>
+        <div class="sk-circle3 sk-circle"></div>
+        <div class="sk-circle4 sk-circle"></div>
+        <div class="sk-circle5 sk-circle"></div>
+        <div class="sk-circle6 sk-circle"></div>
+        <div class="sk-circle7 sk-circle"></div>
+        <div class="sk-circle8 sk-circle"></div>
+        <div class="sk-circle9 sk-circle"></div>
+        <div class="sk-circle10 sk-circle"></div>
+        <div class="sk-circle11 sk-circle"></div>
+        <div class="sk-circle12 sk-circle"></div>        
+    `;
+
+    resultado.appendChild(divSpinner);
 }
