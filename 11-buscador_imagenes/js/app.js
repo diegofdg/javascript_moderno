b9e7cf4b0570156fa6f558906b1698b4d1a12272
diegofdg <diegofdg@gmail.com>
2022-03-27 08:23:49 -0300

    11_buscandor_imagenes-mostrar los resultados de la consulta

--- 11-buscador_imagenes/js/app.js
+++ 11-buscador_imagenes/js/app.js
@@ -39,7 +39,7 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
     const key = '22323363-5b5df4ad5be481008987d9594';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(resultado => {
@@ -48,5 +48,26 @@ function buscarImagenes(termino) {
 }
 
 function mostrarImagenes(imagenes) {
-    console.log(imagenes);
+    limpiarHTML();
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}" >
+                    <div class="p-4">
+                        <p class="font-bold"> ${likes}<span class="font-light"> Me Gusta</span></p>
+                        <p class="font-bold"> ${views}<span class="font-light"> Veces Vista</span></p>
+                        <a class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center mt-5 p-1" href="${largeImageURL}" target="_blank" rel="noopener noreferrer"> Ver Imagen </a>
+                    </div>
+                </div>
+            </div>            
+        `;
+    });
+}
+
+function limpiarHTML() {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
 }
