ea02ff424f7a38f4041d3da5230338e9f7cbbbb3
diegofdg <diegofdg@gmail.com>
2022-03-27 08:47:34 -0300

    11_buscandor_imagenes-mostrando la cantidad de páginas del paginador

--- 11-buscador_imagenes/js/app.js
+++ 11-buscador_imagenes/js/app.js
@@ -1,5 +1,6 @@
 const resultado = document.querySelector('#resultado');
 const formulario = document.querySelector('#formulario');
+const paginacionDiv = document.querySelector('#paginacion');
 
 const registrosPorPagina = 40;
 let totalPaginas;
@@ -43,7 +44,7 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
     const key = '22323363-5b5df4ad5be481008987d9594';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}`;
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(resultado => {
@@ -53,7 +54,7 @@ function buscarImagenes(termino) {
 }
 
 function *crearPaginador(total) {
-    for (let i = 1; i >= total; i++) {
+    for (let i = 1; i <= total; i++) {
         yield i;
     }
 }
@@ -79,15 +80,36 @@ function mostrarImagenes(imagenes) {
             </div>            
         `;
     });
+    limpiarPaginador();
     imprimirPaginador();
 }
 
 function imprimirPaginador() {
     iterador = crearPaginador(totalPaginas);
+
+    while(true) {
+        const { value, done } = iterador.next();
+        if(done) {
+            return;
+        }
+        const boton = document.createElement('a');
+        boton.href = '#';
+        boton.dataset.pagina = value;
+        boton.textContent = value;
+        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'uppercase', 'rounded');
+
+        paginacionDiv.appendChild(boton);        
+    }
 }
 
 function limpiarHTML() {
     while(resultado.firstChild) {
         resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function limpiarPaginador() {
+    while(paginacionDiv.firstChild) {
+        paginacionDiv.removeChild(paginacionDiv.firstChild);
     }
 }
