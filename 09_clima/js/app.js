304e3f9a54af2323f7bac3cb7a07a5f02c84f128
diegofdg <diegofdg@gmail.com>
2022-03-26 21:19:19 -0300

    09_clima-validando el formulario

--- 09_clima/js/app.js
+++ 09_clima/js/app.js
@@ -8,5 +8,29 @@ window.addEventListener('load', () => {
 
 function buscarClima(e) {
     e.preventDefault();
-    console.log('buscando el clima');
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    if(ciudad === '' || pais === '') {
+        mostrarError('Ambos campos son obligatorios');
+        return;
+    }
+}
+
+function mostrarError(mensaje) {
+    const alerta = document.querySelector('.bg-red-100');
+
+    if(!alerta) {
+        const alerta = document.createElement('div');
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+        alerta.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block">${mensaje}</span>
+        `;
+        container.appendChild(alerta);
+
+        setTimeout( () => {
+            alerta.remove();
+        }, 3000);
+    }
 }
