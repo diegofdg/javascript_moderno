b41d2b439591058595a45afe823651c3e80ae099
diegofdg <diegofdg@gmail.com>
2022-03-27 09:59:46 -0300

    12_cotizador_criptomonedas-mostrando un spinner de carga

--- 12-cotizador-criptomonedas/js/app.js
+++ 12-cotizador-criptomonedas/js/app.js
@@ -74,6 +74,9 @@ function consultarAPI() {
     const { moneda, criptomoneda } = objBusqueda;
 
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+    mostrarSpinner();
+
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(cotizacion => {
@@ -112,4 +115,18 @@ function limpiarHTML() {
     while(resultado.firstChild) {
         resultado.removeChild(resultado.firstChild);
     }
+}
+
+function mostrarSpinner() {
+    limpiarHTML();
+
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner');
+
+    spinner.innerHTML = `        
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>        
+    `;
+    resultado.appendChild(spinner);
 }
