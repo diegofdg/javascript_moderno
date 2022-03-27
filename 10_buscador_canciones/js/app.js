8ed1b586a352a431f7a0573197025358fc6aa844
diegofdg <diegofdg@gmail.com>
2022-03-27 07:31:28 -0300

    10_buscandor_canciones-primeros pasos de este proyecto

---
+++ 10_buscador_canciones/js/app.js
@@ -0,0 +1,21 @@
+import * as UI from './interfaz.js';
+
+UI.formularioBuscar.addEventListener('submit', buscarCancion);
+
+function buscarCancion(e) {
+    e.preventDefault();
+
+    const artista = document.querySelector('#artista').value;
+    const cancion = document.querySelector('#cancion').value;
+
+    if(artista === '' || cancion === '') {    
+        UI.divMensajes.textContent = 'Error... Todos los campos son obligatorios';
+        UI.divMensajes.classList.add('error');
+        setTimeout(() => {
+             UI.divMensajes.textContent = '';
+             UI.divMensajes.classList.remove('error');
+        }, 3000);
+
+        return;
+    }
+}
