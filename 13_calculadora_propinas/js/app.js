e9e5eacad5bcb772e4062af94eab90ff26324209
diegofdg <diegofdg@gmail.com>
2022-03-28 20:47:08 -0300

    13_calculadora_propinas-mostrando el pedido en el resumen

--- 13_calculadora_propinas/js/app.js
+++ 13_calculadora_propinas/js/app.js
@@ -162,10 +162,50 @@ function actualizarResumen() {
     const heading = document.createElement('H3');
     heading.textContent = 'Platillos Consumidos';
     heading.classList.add('my-4', 'text-center');
+
+    const grupo = document.createElement('UL');
+    grupo.classList.add('list-group');
+
+    const { pedido } = cliente;
+    pedido.forEach( articulo => {
+        const { nombre, cantidad, precio, id } = articulo;
+
+        const lista = document.createElement('LI');
+        lista.classList.add('list-group-item');
+
+        const nombreEl = document.createElement('h4');
+        nombreEl.classList.add('my-4');
+        nombreEl.textContent = nombre;
+
+        const cantidadEl = document.createElement('P');        
+        cantidadEl.classList.add('fw-bold');
+        cantidadEl.textContent = 'Cantidad: ';
+
+        const cantidadValor = document.createElement('SPAN');
+        cantidadValor.classList.add('fw-normal');
+        cantidadValor.textContent = cantidad;
+
+        const precioEl = document.createElement('P');
+        precioEl.classList.add('fw-bold');
+        precioEl.textContent = 'Precio: ';
+
+        const precioValor = document.createElement('SPAN');
+        precioValor.classList.add('fw-normal');
+        precioValor.textContent = `$${precio}`;
+
+        cantidadEl.appendChild(cantidadValor)
+        precioEl.appendChild(precioValor)
+
+        lista.appendChild(nombreEl);
+        lista.appendChild(cantidadEl);
+
+        grupo.appendChild(lista);
+    })
     
     resumen.appendChild(mesa);
     resumen.appendChild(hora);
     resumen.appendChild(heading);
+    resumen.appendChild(grupo);
 
     contenido.appendChild(resumen);
 
