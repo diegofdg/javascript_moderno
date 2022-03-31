9206b15761512aac06e5c23c5ca53c77333f70e0
diegofdg <diegofdg@gmail.com>
2022-03-31 06:51:36 -0300

    15_cotizador_prestamos-agregando el componente para la cantidad a pagar

--- 15_cotizador_prestamos/src/App.js
+++ 15_cotizador_prestamos/src/App.js
@@ -5,6 +5,7 @@ import Formulario from './componentes/Formulario';
 function App() {
     const [ cantidad, guardarCantidad ] = useState(0);
     const [ plazo, guardarPlazo ] = useState('');
+    const [ total, guardarTotal ] = useState(0);
 
     return (
         <Fragment>
@@ -17,7 +18,10 @@ function App() {
                     guardarCantidad={guardarCantidad}
                     plazo={plazo}
                     guardarPlazo={guardarPlazo}
+                    total={total}
+                    guardarTotal={guardarTotal}
                 />
+                <p>Total a pagar: $ {total}</p>
             </div>
         </Fragment>
     );
--- 15_cotizador_prestamos/src/componentes/Formulario.js
+++ 15_cotizador_prestamos/src/componentes/Formulario.js
@@ -1,7 +1,8 @@
 import React, { Fragment, useState } from 'react';
 import { calcularTotal } from '../helpers';
 
-const Formulario = ({cantidad, guardarCantidad, plazo, guardarPlazo}) => {
+const Formulario = (props) => {
+    const { cantidad, guardarCantidad, plazo, guardarPlazo, total, guardarTotal } = props;
     const [ error, guardarError ] = useState(false);
 
     const leerCantidad = (e) => {
@@ -21,7 +22,7 @@ const Formulario = ({cantidad, guardarCantidad, plazo, guardarPlazo}) => {
         }
         guardarError(false);
         const total = calcularTotal(cantidad, plazo);
-        console.log(total);
+        guardarTotal(total);
     }
 
     return ( 
