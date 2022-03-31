68d82a9db5b96c87ec2e50da0f51b82ddc9dbb55
diegofdg <diegofdg@gmail.com>
2022-03-31 06:09:06 -0300

    15_cotizador_prestamos-pasar datos entre componentes

--- 15_cotizador_prestamos/src/componentes/Header.js
+++ 15_cotizador_prestamos/src/componentes/Header.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const Header = () => {
+const Header = ({ titulo }) => {
     return ( 
-        <h1>Hola Mundo</h1>
+        <Fragment>
+            <h1>{titulo}</h1>            
+        </Fragment>
     );
 }
  
