6337cc896f7ccbc4d412604b4693280da11576b0
diegofdg <diegofdg@gmail.com>
2022-03-31 17:43:10 -0300

    16_agencia_viajes-habilitando la sintaxis de imports y exports

--- 16_agencia_viajes/index.js
+++ 16_agencia_viajes/index.js
@@ -1,8 +1,20 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
 const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.send('Inicio');
+});
+
+app.get('/nosotros', (req, res) => {
+    res.send('Nosotros');
+});
+
+app.get('/contacto', (req, res) => {
+    res.send('Contacto');
+});
 
 app.listen(port, () => {
     console.log(`El servidor está funcionando en el puerto ${port}`);
