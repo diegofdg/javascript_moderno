42aeb8973c91a424dd53f8b50e97040806333c8e
diegofdg <diegofdg@gmail.com>
2022-03-31 23:57:11 -0300

    16_agencia_viajes-consultar la base de datos

--- 16_agencia_viajes/controllers/paginasController.js
+++ 16_agencia_viajes/controllers/paginasController.js
@@ -1,3 +1,5 @@
+import { Viaje }  from '../models/Viaje.js';
+
 const paginaInicio = (req, res) => {
     res.render('inicio', {
         pagina: 'Inicio'
@@ -10,9 +12,12 @@ const paginaNosotros = (req, res) => {
     });
 }
 
-const paginaViajes = (req, res) => {
+const paginaViajes = async (req, res) => {
+    const viajes = await Viaje.findAll();
+
     res.render('viajes', {
-        pagina: 'Viajes'
+        pagina: 'Próximos Viajes',
+        viajes
     });
 }
 
