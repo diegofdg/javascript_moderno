7289d7a8f9ac2c683fe8444ab8d607fbe8db327a
diegofdg <diegofdg@gmail.com>
2022-04-02 01:46:30 -0300

    17_full_stack_js-primeros pasos con el backend

--- 17_full_stack_js/backend/index.js
+++ 17_full_stack_js/backend/index.js
@@ -1,1 +1,11 @@
 import express from 'express';
+
+const app = express();
+
+app.use( (req, res) => {
+    res.send('Hola Mundo');
+});
+
+app.listen(4000, () => {
+    console.log('Servidor funcionando en en el puerto 4000');
+});
