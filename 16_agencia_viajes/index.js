91dee6223eda2fe5a12b659c1d13969f59e33053
diegofdg <diegofdg@gmail.com>
2022-04-02 00:54:55 -0300

    16_agencia_viajes-preparando el proyecto para el deployment-variables de entorno-servidor

--- 16_agencia_viajes/index.js
+++ 16_agencia_viajes/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import router from './routes/index.js';
 import db from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: 'variables.env' });
 
 const app = express();
 
@@ -11,8 +14,6 @@ db.authenticate()
     .catch( error => {
         console.log(error);
     });
-
-const port = process.env.PORT || 4000;
 
 app.set('view engine', 'pug');
 
@@ -28,6 +29,9 @@ app.use(express.static('public'));
 
 app.use('/', router);
 
-app.listen(port, () => {
-    console.log(`El servidor está funcionando en el puerto ${port}`);
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 4000;
+
+app.listen(port, host, () => {
+    console.log(`El servidor está funcionando en ${host} en el puerto ${port}`);
 });
