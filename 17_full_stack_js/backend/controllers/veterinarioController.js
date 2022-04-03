7dfdccb4b2a97ea68720e56f155ef6111fd36d0e
diegofdg <diegofdg@gmail.com>
2022-04-03 00:30:36 -0300

    17_full_stack_js-prevenir registro de usuarios registrados

--- 17_full_stack_js/backend/controllers/veterinarioController.js
+++ 17_full_stack_js/backend/controllers/veterinarioController.js
@@ -1,6 +1,15 @@
 import Veterinario from "../models/Veterinario.js";
 
 const registrar = async (req, res) => {
+    const { email } = req.body;
+
+    const existeUsuario = await Veterinario.findOne({email: email});
+
+    if(existeUsuario){
+        const error = new Error('Usuario ya registrado');
+        return res.status(400).json({msg: error.message});
+    }
+    
     try {
         const veterinario = new Veterinario(req.body);
         const veterinarioGuardado = await veterinario.save();
