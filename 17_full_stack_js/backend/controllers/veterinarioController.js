a3c41eecfc78eaab6ef65d8752f3243c426372c5
diegofdg <diegofdg@gmail.com>
2022-04-04 00:42:56 -0300

    17_full_stack_js-almacenar el nuevo password

--- 17_full_stack_js/backend/controllers/veterinarioController.js
+++ 17_full_stack_js/backend/controllers/veterinarioController.js
@@ -101,6 +101,25 @@ const comprobarToken = async (req, res) => {
     }
 }
 
-const nuevoPassword = (req, res) => { }
+const nuevoPassword = async (req, res) => {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const veterinario = await Veterinario.findOne({token});
+    if(!veterinario) {
+        const error = new Error('Hubo un error');
+        return res.status(400).json({msg: error.message});
+    }
+
+    try {
+        veterinario.token = null;
+        veterinario.password = password;
+        await veterinario.save();
+        res.json({msg: 'Password modificado correctamente'});        
+    } catch (error) {
+        console.log(error);
+    }
+
+}
 
 export { registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword }
