0dbc6028da3f749db066c0b26ee856f6b53301e1
diegofdg <diegofdg@gmail.com>
2022-04-05 18:02:45 -0300

    17_full_stack_js-creando la página de registro de usuarios

--- 17_full_stack_js/frontend/src/paginas/Login.jsx
+++ 17_full_stack_js/frontend/src/paginas/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
             <div>
                 <h1 className="text-indigo-600 font-black text-6xl">Inicia Sesión y Administra tus <span className="text-black">Pacientes</span></h1>
             </div>
-            <div>
+            <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
                 <form>
                     <div className="my-5">
                         <label
