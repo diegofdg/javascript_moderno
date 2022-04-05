13d1578fbc2426e5662b3ba58024575731760dca
diegofdg <diegofdg@gmail.com>
2022-04-04 21:09:43 -0300

    17_full_stack_js-eliminar un paciente

--- 17_full_stack_js/backend/controllers/pacienteController.js
+++ 17_full_stack_js/backend/controllers/pacienteController.js
@@ -52,17 +52,30 @@ const actualizarPaciente = async (req, res) => {
 
     try {
         const pacienteActualizado = await paciente.save();
-        res.json(pacienteActualizado);
-        
+        res.json(pacienteActualizado);        
     } catch (error) {
         console.log(error);
-        
-        
+    }
+}
+
+const eliminarPaciente = async (req, res) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+
+    if(!paciente) {        
+        return res.status(404).json({msg: "No encontrado"});
     }
 
-    
+    if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({msg: 'Acción no válida'});
+    }
 
+    try {
+        await paciente.deleteOne();
+        res.json({msg: 'Paciente eliminado'});        
+    } catch (error) {
+        console.log(error);
+    }
 }
-const eliminarPaciente = async (req, res) => {}
 
 export { agregarPaciente, obtenerPacientes, obtenerPaciente, actualizarPaciente, eliminarPaciente }
