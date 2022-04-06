2536626a3acbed4ce5368197cc6fc860eb0a871d
diegofdg <diegofdg@gmail.com>
2022-04-06 00:18:28 -0300

    17_full_stack_js-confirmando la cuenta del usuario

--- 17_full_stack_js/frontend/src/paginas/Login.jsx
+++ 17_full_stack_js/frontend/src/paginas/Login.jsx
@@ -30,6 +30,7 @@ const Login = () => {
                             type="password"
                             placeholder="Tu password"
                             className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                            autoComplete="on"
                         />
                     </div>
                     <input 
