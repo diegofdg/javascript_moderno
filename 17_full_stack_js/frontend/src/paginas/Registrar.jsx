8d64ac8697d54eb0f3143d72f616e11b78964f24
diegofdg <diegofdg@gmail.com>
2022-04-06 20:31:47 -0300

    17_full_stack_js-creando variables de entorno

--- 17_full_stack_js/frontend/src/paginas/Registrar.jsx
+++ 17_full_stack_js/frontend/src/paginas/Registrar.jsx
@@ -31,7 +31,7 @@ const Registrar = () => {
         setAlerta({});
 
         try {
-            const url = "http://localhost:4000/api/veterinarios"
+            const url = `${import.meta.env.VITE_BACKEND_URL}/api/veterinarios`
             await axios.post(url, { nombre, email, password });
             setAlerta({
                 msg: 'Creado correctamente, revisa tu email',
