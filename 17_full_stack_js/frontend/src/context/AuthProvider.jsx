8f5769c55026ace22c8d86809ecfefefdca3a1fc
diegofdg <diegofdg@gmail.com>
2022-04-07 23:25:11 -0300

    17_full_stack_js-creando un custom hook y accediendo a los datos del context

--- 17_full_stack_js/frontend/src/context/AuthProvider.jsx
+++ 17_full_stack_js/frontend/src/context/AuthProvider.jsx
@@ -7,11 +7,15 @@ const AuthProvider = ({ children }) => {
     const [ auth, setAuth ] = useState({});
 
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider
+            value={{
+                auth,
+                setAuth
+            }}
+        >
             {children}
         </AuthContext.Provider>
-    )
-
+    );
 }
 
 export { AuthProvider }
