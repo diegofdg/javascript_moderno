6a9a1f401684ac09d8b9d4076efb29dd02c287c7
diegofdg <diegofdg@gmail.com>
2022-04-07 23:38:44 -0300

    17_full_stack_js-autenticación de usuarios-primeros pasos

--- 17_full_stack_js/frontend/src/paginas/Login.jsx
+++ 17_full_stack_js/frontend/src/paginas/Login.jsx
@@ -1,8 +1,39 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
+import Alerta from '../components/Alerta';
+import clienteAxios from '../config/axios';
 
 const Login = () => {
-    const { auth } = useAuth();
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ alerta, setAlerta ] = useState({});
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        if([email, password].includes('')) {
+            setAlerta({
+                msg: 'Todos los campos son obligatorios',
+                error: true
+            });
+            return;
+        }
+
+        try {
+            const url = `/veterinarios/login`;
+            const { data } = await clienteAxios.post(url, { email, password });
+            localStorage.setItem('token', data.token);
+            
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true 
+            });            
+        }
+    }
+
+    const { msg } = alerta;
 
     return ( 
         <>
@@ -10,7 +41,12 @@ const Login = () => {
                 <h1 className="text-indigo-600 font-black text-6xl">Inicia Sesión y Administra tus <span className="text-black">Pacientes</span></h1>
             </div>
             <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-                <form>
+                { msg && <Alerta 
+                    alerta={alerta}
+                />}
+                <form
+                    onSubmit={handleSubmit}
+                >
                     <div className="my-5">
                         <label
                             className="uppercase text-gray-600 block text-xl font-bold"
@@ -21,6 +57,8 @@ const Login = () => {
                             type="email"
                             placeholder="Email de Registro"
                             className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                            value={email}
+                            onChange={ e => setEmail(e.target.value)}
                         />
                     </div>
                     <div className="my-5">
@@ -34,6 +72,8 @@ const Login = () => {
                             placeholder="Tu password"
                             className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
                             autoComplete="on"
+                            value={password}
+                            onChange={ e => setPassword(e.target.value)}
                         />
                     </div>
                     <input 
