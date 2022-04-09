3178816de429d4b1b36db3fce2f7cf0dc333a80e
diegofdg <diegofdg@gmail.com>
2022-04-09 17:29:48 -0300

    17_full_stack_js-primeros pasos mostrando a los pacientes

--- 17_full_stack_js/frontend/src/components/Formulario.jsx
+++ 17_full_stack_js/frontend/src/components/Formulario.jsx
@@ -30,7 +30,8 @@ const Formulario = () => {
 
     return (
         <>
-            <p className="text-lg text-center mb-10">Añade tus pacientes y <span className="text-indigo-600 font-bold">Administralos</span></p>
+            <h2 className="font-black text-3xl text-center">Administrador de Pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">Añade tus pacientes y <span className="text-indigo-600 font-bold">Administralos</span></p>
             <form
                 className="bg-white py-10 px-5 mb-10 lg:mb-5 shadow-md rounded-md"
                 onSubmit={handleSubmit}
--- 17_full_stack_js/frontend/src/components/ListadoPacientes.jsx
+++ 17_full_stack_js/frontend/src/components/ListadoPacientes.jsx
@@ -1,4 +1,5 @@
 import usePacientes from "../hooks/usePacientes";
+import Paciente from "./Paciente";
 
 const ListadoPacientes = () => {    
     const { pacientes } = usePacientes();
@@ -11,6 +12,13 @@ const ListadoPacientes = () => {
                     <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
                     
                     <p className="text-xl mt-5 mb-10 text-center">Administra tus <span className="text-indigo-600 font-bold">Pacientes y Citas</span></p>
+
+                    {pacientes.map( paciente => (
+                        <Paciente
+                            key={paciente._id}
+                            paciente={paciente}
+                        />
+                    ))}
                 </>
             ) : 
             (
