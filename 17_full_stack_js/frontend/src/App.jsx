069c684aece2a4a940e48acd014455ba99ea6dec
diegofdg <diegofdg@gmail.com>
2022-04-09 09:34:40 -0300

    17_full_stack_js-creando un context de pacientes

--- 17_full_stack_js/frontend/src/App.jsx
+++ 17_full_stack_js/frontend/src/App.jsx
@@ -8,23 +8,26 @@ import ConfirmarCuenta from './paginas/ConfirmarCuenta';
 import NuevoPassword from './paginas/NuevoPassword';
 import AdministrarPacientes from './paginas/AdministrarPacientes';
 import { AuthProvider } from './context/AuthProvider';
+import { PacientesProvider } from './context/PacientesProvider';
 
 function App() {
     return (
         <BrowserRouter>
             <AuthProvider>
-                <Routes>
-                    <Route path="/" element={<AuthLayout />}>
-                        <Route index element={<Login />}/>
-                        <Route path="registrar" element={<Registrar />}/>
-                        <Route path="olvide-password" element={<OlvidePassword />}/>
-                        <Route path="olvide-password/:token" element={<NuevoPassword />}/>
-                        <Route path="confirmar/:id" element={<ConfirmarCuenta />}/>
-                    </Route>
-                    <Route path="/admin" element={<RutaProtegida />}>
-                        <Route index element={<AdministrarPacientes />}/>
-                    </Route>
-                </Routes>
+                <PacientesProvider>
+                    <Routes>
+                        <Route path="/" element={<AuthLayout />}>
+                            <Route index element={<Login />}/>
+                            <Route path="registrar" element={<Registrar />}/>
+                            <Route path="olvide-password" element={<OlvidePassword />}/>
+                            <Route path="olvide-password/:token" element={<NuevoPassword />}/>
+                            <Route path="confirmar/:id" element={<ConfirmarCuenta />}/>
+                        </Route>
+                        <Route path="/admin" element={<RutaProtegida />}>
+                            <Route index element={<AdministrarPacientes />}/>
+                        </Route>
+                    </Routes>
+                </PacientesProvider>
             </AuthProvider>
         </BrowserRouter>
     );
