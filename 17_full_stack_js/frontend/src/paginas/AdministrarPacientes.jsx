2c6f25f72226d791f24697a48c1de7e7cd702d5a
diegofdg <diegofdg@gmail.com>
2022-04-09 07:08:11 -0300

    17_full_stack_js-creando el área de pacientes

--- 17_full_stack_js/frontend/src/paginas/AdministrarPacientes.jsx
+++ 17_full_stack_js/frontend/src/paginas/AdministrarPacientes.jsx
@@ -1,6 +1,16 @@
+import ListadoPacientes from "../components/ListadoPacientes";
+import Formulario from "../components/Formulario";
+
 const AdministrarPacientes = () => {
     return (
-        <h1>AdministrarPacientes</h1>
+        <div className="flex flex-col md:flex-row">
+            <div className="md:w-1/2 lg:w-2/5">
+                <Formulario />
+            </div>
+            <div className="md:w-1/2 lg:w-3/5">
+                <ListadoPacientes />
+            </div>
+        </div>
     );
 }
 
