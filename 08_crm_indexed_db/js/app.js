b16b25eda261cc09951e778b4813c40f77c8b158
diegofdg <diegofdg@gmail.com>
2022-05-01 09:40:14 -0300

    08_crm_indexed_db-ajuste en propiedad css duplicada

--- 08_crm_indexed_db/js/app.js
+++ 08_crm_indexed_db/js/app.js
@@ -75,7 +75,7 @@
                     listadoClientes.innerHTML += `
                         <tr>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                                <p class="leading-5 text-gray-700 text-lg  font-bold"> ${nombre} </p>
                                 <p class="text-sm leading-10 text-gray-700"> ${email} </p>
                             </td>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
