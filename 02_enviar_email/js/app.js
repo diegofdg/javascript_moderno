ec887718826e3735486e9772e047c4ada2102e67
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-25 11:31:04 -0300

    02_enviar_email-primeros pasos con el proyecto

---
+++ 02_enviar_email/js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Seleccionar los elementos de la interfaz
+  const inputEmail = document.querySelector('#email');
+  const inputAsunto = document.querySelector('#asunto');
+  const inputMensaje = document.querySelector('#mensaje');
+
+});
