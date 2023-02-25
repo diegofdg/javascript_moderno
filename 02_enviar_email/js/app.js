472fa5070efb334ff0d4cb26509af5cf556d427b
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-25 18:08:48 -0300

    02_enviar_email-detectando cuando un campo está vacío

--- 02_enviar_email/js/app.js
+++ 02_enviar_email/js/app.js
@@ -11,7 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
   inputMensaje.addEventListener('blur', validar);
 
   function validar(e) {
-    console.log(e.target.value);
+    if(e.target.value.trim() === '') {
+      console.log("está vacío");
+    } else {
+      console.log("tiene algo");
+    }
   }
 
 });
