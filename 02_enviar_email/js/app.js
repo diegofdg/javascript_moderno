94cdfdc0b282be6452ed58ec83ea6586a0c79371
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-25 18:39:22 -0300

    02_enviar_email-mostrando las alertas junto a sus campos

--- 02_enviar_email/js/app.js
+++ 02_enviar_email/js/app.js
@@ -13,20 +13,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function validar(e) {
     if(e.target.value.trim() === '') {
-      mostrarAlerta(`El Campo ${e.target.id} es obligatorio`);
+      mostrarAlerta(`El Campo ${e.target.id} es obligatorio`, e.target.parentElement);
     } else {
       console.log("tiene algo");
     }
   }
 
-  function mostrarAlerta(mensaje) {
+  function mostrarAlerta(mensaje, referencia) {
     // Generar una alerta en HTML
     const error = document.createElement('P');
     error.textContent = mensaje;
     error.classList.add('bg-red-600', 'text-white', 'p-2');
     
     // Inyectar el error al formulario
-    formulario.appendChild(error);
+    referencia.appendChild(error);
   }
 
 });
