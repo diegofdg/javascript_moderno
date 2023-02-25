79b861fba4bddad2d85cfb37a5954c71c7dacd76
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-25 18:20:57 -0300

    02_enviar_email-añadiendo la alerta en el html

--- 02_enviar_email/js/app.js
+++ 02_enviar_email/js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const inputEmail = document.querySelector('#email');
   const inputAsunto = document.querySelector('#asunto');
   const inputMensaje = document.querySelector('#mensaje');
+  const formulario = document.querySelector('#formulario');
 
   // Asignar eventos
   inputEmail.addEventListener('blur', validar);
@@ -22,7 +23,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // Generar una alerta en HTML
     const error = document.createElement('P');
     error.textContent = 'Hubo un error';
-    console.log(error);
+    error.classList.add('bg-red-600', 'text-white', 'p-2');
+    
+    // Inyectar el error al formulario
+    formulario.appendChild(error);
   }
 
 });
