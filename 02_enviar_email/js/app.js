60d83f1093e79acf49c00e4d327c3c1f6516936f
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-26 16:53:28 -0300

    02_enviar_email-habilitar o deshabilitar el botón de enviar

--- 02_enviar_email/js/app.js
+++ 02_enviar_email/js/app.js
@@ -11,20 +11,25 @@ document.addEventListener('DOMContentLoaded', function() {
   const inputAsunto = document.querySelector('#asunto');
   const inputMensaje = document.querySelector('#mensaje');
   const formulario = document.querySelector('#formulario');
+  const btnSubmit = document.querySelector('#formulario button[type="submit"]');
 
   // Asignar eventos
-  inputEmail.addEventListener('blur', validar);
-  inputAsunto.addEventListener('blur', validar);
-  inputMensaje.addEventListener('blur', validar);
+  inputEmail.addEventListener('input', validar);
+  inputAsunto.addEventListener('input', validar);
+  inputMensaje.addEventListener('input', validar);
 
   function validar(e) {
     if(e.target.value.trim() === '') {
       mostrarAlerta(`El Campo ${e.target.id} es obligatorio`, e.target.parentElement);
+      email[e.target.name] = '';
+      comprobarEmail();
       return;
     }
 
     if(e.target.id === 'email' && !validarEmail(e.target.value)) {
       mostrarAlerta('El email no es válido', e.target.parentElement);
+      email[e.target.name] = '';
+      comprobarEmail();
       return;
     };
 
@@ -65,6 +70,12 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   function comprobarEmail() {
-    console.log(Object.values(email).includes(''));    
+    if(Object.values(email).includes('')) {
+      btnSubmit.classList.add('opacity-50');
+      btnSubmit.disabled = true;
+      return;
+    } 
+    btnSubmit.classList.remove('opacity-50');
+    btnSubmit.disabled = false;
   }
 });
