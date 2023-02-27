83613876a6758c43cc07b8c6d7726043efdf3e7c
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-27 19:34:36 -0300

    10_buscador_recetas-mostrando ingredientes y cantidades en el modal

--- 10_buscador_recetas/js/app.js
+++ 10_buscador_recetas/js/app.js
@@ -108,7 +108,27 @@ function iniciarApp() {
       <img class="img-fluid" src="${strMealThumb}" alt="receta ${strMeal}" />
       <h3 class="my-3">Instrucciones</h3>
       <p>${strInstructions}</p>
-    `;    
+      <h3 class="my-3">Ingredientes y Cantidades</h3>
+    `;
+
+    const listGroup = document.createElement('UL');
+    listGroup.classList.add('list-group');
+
+    // Mostrar cantidades e ingredientes
+    for(let i = 1; i <= 20; i++ ) {
+      if(receta[`strIngredient${i}`]) {
+        const ingrediente = receta[`strIngredient${i}`];
+        const cantidad = receta[`strMeasure${i}`];
+
+        const ingredienteLi = document.createElement('LI');
+        ingredienteLi.classList.add('list-group-item');
+        ingredienteLi.textContent = `${ingrediente} - ${cantidad}`
+
+        listGroup.appendChild(ingredienteLi);
+        }
+    }
+
+    modalBody.appendChild(listGroup);
 
     // Muestra el modal
     modal.show();
