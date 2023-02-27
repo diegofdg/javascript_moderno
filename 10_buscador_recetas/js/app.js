d7d89b44f44d0c7ac80fed4ce6795794a0ef9857
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-27 07:29:44 -0300

    10_buscador_recetas-mostrando información en el modal

--- 10_buscador_recetas/js/app.js
+++ 10_buscador_recetas/js/app.js
@@ -3,6 +3,7 @@ function iniciarApp() {
   const selectCategoria = document.querySelector('#categorias');
   selectCategoria.addEventListener('change', seleccionarCategoria);
   const resultado = document.querySelector('#resultado');
+  const modal = new bootstrap.Modal('#modal', {});
 
   obtenerCategorias();
 
@@ -95,8 +96,22 @@ function iniciarApp() {
   }
 
   function mostrarRecetaModal(receta) {
-    
 
+    const { idMeal, strInstructions, strMeal, strMealThumb } = receta;
+        
+    // Añadir contenido al modal
+    const modalTitle = document.querySelector('.modal .modal-title');
+    const modalBody = document.querySelector('.modal .modal-body');
+
+    modalTitle.textContent = strMeal;
+    modalBody.innerHTML = `
+      <img class="img-fluid" src="${strMealThumb}" alt="receta ${strMeal}" />
+      <h3 class="my-3">Instrucciones</h3>
+      <p>${strInstructions}</p>
+    `;    
+
+    // Muestra el modal
+    modal.show();
   }
 
   function limpiarHtml(selector) {
