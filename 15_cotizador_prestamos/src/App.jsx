5f360f459f8bb05592d55ae313f8be92a412662e
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-28 06:08:56 -0300

    15_cotizador_prestamos-cómo modificar el state en react

--- 15_cotizador_prestamos/src/App.jsx
+++ 15_cotizador_prestamos/src/App.jsx
@@ -3,7 +3,11 @@ import Header from "./components/Header";
 
 function App() {
   const [ cantidad, setCantidad ] = useState(10000);
-  
+
+  function handleChange(e) {
+    console.log(Number(e.target.value));
+  }
+
   
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
@@ -11,6 +15,7 @@ function App() {
       <input
         type="range"
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={handleChange}
       />
     </div>
   )
