dc9b91b1da38126d8d1b21f24de06261b2760b74
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-28 05:54:13 -0300

    15_cotizador_prestamos-componentes en react

--- 15_cotizador_prestamos/src/App.jsx
+++ 15_cotizador_prestamos/src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
+
 function App() {
   
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-      
+      <Header />
     </div>
   )
 }
