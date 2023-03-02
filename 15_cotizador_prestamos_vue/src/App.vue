259501389e2d7101ae3e40030f969968276ec67d
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-02 19:10:50 -0300

    15_cotizador_prestamos-componentes en vue

--- 15_cotizador_prestamos_vue/src/App.vue
+++ 15_cotizador_prestamos_vue/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-
+  import Header from './components/Header.vue';
 </script>
 
 <template>
-  <h1 class="text-4xl font-black">Hola mundo!!!</h1>
+  <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+    <Header />
+  </div>
 </template>
