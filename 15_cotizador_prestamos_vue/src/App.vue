b88f10d73ca6d0aff8dfae7b7244f7f6adb80097
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-02 20:36:00 -0300

    15_cotizador_prestamos-eventos en vue.js

--- 15_cotizador_prestamos_vue/src/App.vue
+++ 15_cotizador_prestamos_vue/src/App.vue
@@ -1,10 +1,8 @@
-<script>
+<script setup>
   import Header from './components/Header.vue';
 
-  export default {
-    components: {
-      Header
-    }
+  function handleChange(e) {
+    console.log(e.target.value);
   }
   
 </script>
@@ -12,5 +10,13 @@
 <template>
   <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
+
+    <div class="my-5">
+      <input
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        v-on:input="handleChange"
+      />
+    </div>
   </div>
 </template>
