5d2360717af561cce4b253297e4cf4ee0ceadafb
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-01 21:28:00 -0300

    15_cotizador_prestamos-qué es el useeffect y cómo se usa

--- 15_cotizador_prestamos/src/App.jsx
+++ 15_cotizador_prestamos/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Button from "./components/Button";
 import { formatearDinero, calcularTotalPagar } from "./helpers";
@@ -7,6 +7,11 @@ function App() {
   const [ cantidad, setCantidad ] = useState(10000);
   const [ meses, setMeses ] = useState(6);
   const [ total, setTotal ] = useState(0);
+
+  useEffect(()=>{
+    const resultadoTotalPagar = calcularTotalPagar(cantidad, meses);
+    setTotal(resultadoTotalPagar);
+  },[cantidad, meses])
 
   const MIN = 0;
   const MAX = 20000;
@@ -83,7 +88,7 @@ function App() {
           Resumen <span className="text-indigo-600">de pagos</span>
         </h2>
         <p className="text-xl text-gray-500 text-center font-bold">{meses} Meses</p>
-        <p className="text-xl text-gray-500 text-center font-bold">Total a Pagar</p>
+        <p className="text-xl text-gray-500 text-center font-bold">{formatearDinero(total)} Total a Pagar</p>
         <p className="text-xl text-gray-500 text-center font-bold">Mensuales</p>
 
 
